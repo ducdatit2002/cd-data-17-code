a6c0bda38698b5d4b1e5df285dbebdab765c6bfc
ducdatit2002 <ducdatit2002@gmail.com>
2026-09-22 21:23:37 +0700

    Update 22092026

---
+++ buoi_08/read_docx.py
@@ -0,0 +1,24 @@
+# muốn đọc file cover_letter/Le_Van_C.docx
+
+from docx import Document
+
+file_path = "cover_letters/le_van_c.docx"
+doc = Document(file_path)
+print(doc)
+print(doc.paragraphs[3].text)
+
+# Cấu trúc: Document -> Paragraphs -> List các paragraph object -> Paragraph.text -> nội dung dạng string
+
+# doc_content = []
+
+# for paragraph in doc.paragraphs:
+#     doc_content.append(paragraph.text)
+
+# print(doc_content)
+
+doc_content = [
+    paragraph.text
+    for paragraph in doc.paragraphs
+]
+doc_full = "\n".join(doc_content)
+print(doc_full)
