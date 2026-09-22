7733575a442e93f720dfe0eac886b7edee4b8b8c
ducdatit2002 <ducdatit2002@gmail.com>
2026-09-22 20:11:14 +0700

    Update 22092026

--- buoi_07/visitor_sorted.py
+++ buoi_07/visitor_sorted.py
@@ -35,3 +35,29 @@ for row_index, row in enumerate(sorted_data, start=2):
         )
 # B5: Lưu thành file mới 
 workbook.save("visitor_data_sorted.xlsx")
+
+# Một số lỗi thường gặp:
+# 1. Sai tên file: visitor_data thay vì visitor_data.xlsx
+# 2. ModuleNotFoundError: cài openpyxl ở môi trường khác, chạy ở môi trường khác
+# 3. File Excel đang mở -> tắt file xong mở lại 
+# 4. Sai indentation -> chú ý mối quan hệ giữa if và else 
+# 5. Lỗi quên tăng row_num (quên thêm row_num += 1)
+# 6. Nhầm index của Python và vị trí Excel 
+
+# Tổng kết nội dung buổi 7:
+# 1. Hiểu được cập nhật dữ liệu Excel: 
+# - đọc file excel bằng openpyxl 
+# - kiểm tra bằng os
+# - thêm cột, đọc từng hàng
+# - dùng if,else
+# - cập nhật và lưu file excel 
+
+# 2. Bubble Sort
+# - so sánh 2 phần tử cạnh nhau
+# - sai thứ tự thì hoán đổi
+# - vì sao cần vòng lặp lồng nhau
+# - sắp xếp tăng dần, giảm dần
+
+# 3. Ứng dụng Bubble Sore vào Excel 
+# - Chuyển từ "Danh sách 1 chiều" -> "Bảng dữ liệu 2 chiều"
+
