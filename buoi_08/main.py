7ddacb177c23b3b80729c5c7dcb3a5b949692bae
ducdatit2002 <ducdatit2002@gmail.com>
2026-09-24 19:24:17 +0700

    Update 22092026

--- buoi_08/main.py
+++ buoi_08/main.py
@@ -1,9 +1,7 @@
 from cover_letter_processor import CoverLetterProcessor
 
 processor = CoverLetterProcessor(
-    "cover_letter",
-    "so_yeu_ly_lich.xlsx"
+    folder_path="cover_letter",
+    excel_file="so_yeu_ly_lich.xlsx"
 )
-processor.initialize_excel()
-processor.wb.save(processor.excel_file)
-
+processor.process_documents()
